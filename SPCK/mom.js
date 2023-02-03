11fdfb4151ecc9220a3f999064d10b7a04bba6fc
thanhoeydat <thanhdat03022007@gmail.com>
2023-02-03 23:55:18 +0700

    spck

---
+++ SPCK/mom.js
@@ -0,0 +1,5 @@
+function myFunction() {
+  alert(
+    "Chúc mẹ ngày 8/3 vui vẻ, trẻ khỏe và cùng nhau xây dựng tổ ấm chúng ta ngày càng êm đềm và ngập tràn tình yêu thương. Mẹ Dương là số 1!"
+  );
+}
---
+++ SPCK/shop.js
@@ -0,0 +1,30 @@
+let element1 = document.getElementById("myBtn1");
+element1.addEventListener("click", myFunction1);
+function myFunction1() {
+  document.getElementById("demo").innerHTML += "giày em bé<br> ";
+}
+let element2 = document.getElementById("myBtn2");
+element2.addEventListener("click", myFunction2);
+function myFunction2() {
+  document.getElementById("demo").innerHTML += "trà sữa<br> ";
+}
+let element3 = document.getElementById("myBtn3");
+element3.addEventListener("click", myFunction3);
+function myFunction3() {
+  document.getElementById("demo").innerHTML += "Chè thập cẩm<br> ";
+}
+let element4 = document.getElementById("myBtn4");
+element4.addEventListener("click", myFunction4);
+function myFunction4() {
+  document.getElementById("demo").innerHTML += "đồ trang sức<br> ";
+}
+let element5 = document.getElementById("myBtn5");
+element5.addEventListener("click", myFunction5);
+function myFunction5() {
+  document.getElementById("demo").innerHTML += "bỉm<br> ";
+}
+let element6 = document.getElementById("myBtn6");
+element6.addEventListener("click", myFunction6);
+function myFunction6() {
+  document.getElementById("demo").innerHTML += "skincare<br> ";
+}
