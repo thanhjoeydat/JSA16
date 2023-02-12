33271bcaa1cab6fa4d77d1387b08490ee2e470c0
thanhoeydat <thanhdat03022007@gmail.com>
2023-02-12 11:14:48 +0700

    update spck

--- SPCK/shop.js
+++ SPCK/shop.js
@@ -1,30 +1,76 @@
-let element1 = document.getElementById("myBtn1");
-element1.addEventListener("click", myFunction1);
-function myFunction1() {
-  document.getElementById("demo").innerHTML += "giày em bé<br> ";
+let btn = document.querySelectorAll("button");
+btn.forEach(function (button, index) {
+  button.addEventListener("click", function (event) {
+    let btnItem = event.target;
+    let product = btnItem.parentElement;
+    let productimg = product.querySelector("img").src;
+    let productname = product.querySelector("h4").innerText;
+    let productprice = product.querySelector(".priceitem").innerText;
+    Addcart(productprice, productimg, productname);
+  });
+});
+function Addcart(productprice, productimg, productname) {
+  let addtr = document.createElement("tr");
+  let cartItem = document.querySelectorAll("tbody tr");
+  for (let i = 0; i < cartItem.length; i++) {
+    let productT = document.querySelectorAll(".title");
+    if (productT[i].innerHTML == productname) {
+      alert("Sản phẩm đã có trong giỏ hàng");
+      return;
+    }
+  }
+  let trcontent =
+    '<tr><td style="display: flex; align-items: center" class="tdbim"><img class="picbim" src="' +
+    productimg +
+    '" /><p class ="title">' +
+    productname +
+    "</p></td><td><i><span><b>" +
+    productprice +
+    '</b></span><b><sup>đ</sup></b></i></td><td><input style="width: 50px; outline: none"class="inputbim"type="number"value="1"min="1"/></td><td style="cursor: pointer" class="deletebim">Xóa</td></tr>';
+  addtr.innerHTML = trcontent;
+  let cartTable = document.querySelector("tbody");
+  cartTable.append(addtr);
+  cartTotal();
+  deleteCart();
 }
-let element2 = document.getElementById("myBtn2");
-element2.addEventListener("click", myFunction2);
-function myFunction2() {
-  document.getElementById("demo").innerHTML += "trà sữa<br> ";
+function cartTotal() {
+  let cartItem = document.querySelectorAll("tbody tr");
+  let total1C = 0;
+  for (let i = 0; i < cartItem.length; i++) {
+    let inputvalue = cartItem[i].querySelector("input").value;
+    let productprice = cartItem[i].querySelector("span").innerText;
+    let total1A = inputvalue * productprice * 1000;
+    total1C = total1C + total1A;
+    // total1D = total1C.toLocaleString("de-DE");
+  }
+  let cartTotalA = document.querySelector(".totalcart span");
+  let cartPrice = document.querySelector(".cart-icon span");
+  cartTotalA.innerHTML = total1C.toLocaleString("de-DE");
+  cartPrice.innerHTML = total1C.toLocaleString("de-DE");
+  inputchange();
 }
-let element3 = document.getElementById("myBtn3");
-element3.addEventListener("click", myFunction3);
-function myFunction3() {
-  document.getElementById("demo").innerHTML += "Chè thập cẩm<br> ";
+
+function deleteCart() {
+  let cartItem = document.querySelectorAll("tbody tr");
+  for (let i = 0; i < cartItem.length; i++) {
+    let productT = document.querySelectorAll(".deletebim");
+    productT[i].addEventListener("click", function (event) {
+      let cartDelete = event.target;
+      let cartItem2 = cartDelete.parentElement;
+      cartItem2.remove();
+      cartTotal();
+    });
+  }
 }
-let element4 = document.getElementById("myBtn4");
-element4.addEventListener("click", myFunction4);
-function myFunction4() {
-  document.getElementById("demo").innerHTML += "đồ trang sức<br> ";
+function inputchange() {
+  let cartItem = document.querySelectorAll("tbody tr");
+  for (let i = 0; i < cartItem.length; i++) {
+    let inputvalue = cartItem[i].querySelector("input");
+    inputvalue.addEventListener("change", function () {
+      cartTotal();
+    });
+  }
 }
-let element5 = document.getElementById("myBtn5");
-element5.addEventListener("click", myFunction5);
-function myFunction5() {
-  document.getElementById("demo").innerHTML += "bỉm<br> ";
-}
-let element6 = document.getElementById("myBtn6");
-element6.addEventListener("click", myFunction6);
-function myFunction6() {
-  document.getElementById("demo").innerHTML += "skincare<br> ";
-}
+
+let cartbtn = document.querySelector(".fa-solid fa-x");
+let cartshow = document.querySelector(".fa-solid fa-cart-shopping");
